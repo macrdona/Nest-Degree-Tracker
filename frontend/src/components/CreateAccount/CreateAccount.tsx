b60363fda893dbb862de9bab7031bc219b43ab8f
Ricky LeDew <adotout1@gmail.com>
2023-02-18 19:14:42 -0500

    Improve page styles, more bootstrap classes

--- frontend/src/components/CreateAccount/CreateAccount.tsx
+++ frontend/src/components/CreateAccount/CreateAccount.tsx
@@ -47,76 +47,96 @@ function CreateAccount() {
   };
 
   return (
-    <div className="container-fluid">
-      <div className="row no-gutters">
-        <div className="col"></div>
-        <div className="col-8">
-          <h1>Create Account</h1>
-          <div className="container d-flex justify-content-center">
-            <form className="form">
-              <div className="form-group">
-                <label htmlFor="formGroupExampleInput">Name</label>
+    <div className="container-fluid flex-fill">
+      <div className="row no-gutters h-100 flex-fill">
+        <div className="d-none d-lg-flex col background-container"></div>
+        <div className="col col-lg-6 p-5 d-flex flex-column align-items-center shadow-lg">
+          <h1 className="display-1 mt-5 text-secondary">Create Account</h1>
+          <div className="d-flex m-5 w-50">
+            <form className="form d-flex flex-column gap-2 w-100">
+              <div className="form-group w-100">
+                <label
+                  htmlFor="formGroupExampleInput"
+                  className="form-label fs-5"
+                >
+                  Name
+                </label>
                 <input
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                   type="text"
-                  className="form-control"
+                  className="form-control form-control-lg"
                   id="name"
                   required
                   autoComplete="off"
                 />
               </div>
 
-              <div className="form-group">
-                <label htmlFor="formGroupExampleInput">Email</label>
+              <div className="form-group w-100">
+                <label
+                  htmlFor="formGroupExampleInput"
+                  className="form-label fs-5"
+                >
+                  Email
+                </label>
                 <input
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   type="text"
-                  className="form-control"
+                  className="form-control form-control-lg"
                   id="email"
                   required
                   autoComplete="off"
                 />
               </div>
 
-              <div className="form-group">
-                <label htmlFor="formGroupExampleInput">Password</label>
+              <div className="form-group w-100">
+                <label
+                  htmlFor="formGroupExampleInput"
+                  className="form-label  fs-5"
+                >
+                  Password
+                </label>
                 <input
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   type="text"
-                  className="form-control"
+                  className="form-control form-control-lg"
                   id="password"
                   required
                   autoComplete="off"
                 />
               </div>
 
-              <div className="form-group">
-                <label htmlFor="formGroupExampleInput">Confirm Password</label>
+              <div className="form-group w-100">
+                <label
+                  htmlFor="formGroupExampleInput"
+                  className="form-label  fs-5"
+                >
+                  Confirm Password
+                </label>
                 <input
                   value={confirmPassword}
                   onChange={(e) => setConfirmPassword(e.target.value)}
                   type="text"
-                  className="form-control"
+                  className="form-control form-control-lg"
                   id="confirmPassword"
                   required
                   autoComplete="off"
                 />
               </div>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleSubmit();
+                }}
+                className="btn btn-primary btn-lg mt-3 fs-2 mw-75 align-self-center text-nowrap"
+                type="submit"
+              >
+                Create Account
+              </button>
             </form>
           </div>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              handleSubmit();
-            }}
-            className="button text-white"
-            type="submit"
-          >
-            Create Account
-          </button>
         </div>
       </div>
     </div>
