8627b8aca233b6f996a541bb403214828a50d0c5
Patrick <adotout1@gmail.com>
2023-03-09 12:21:59 -0500

    Add useLogin query hook

--- frontend/src/lib/api/useLogin.ts
+++ frontend/src/lib/api/useLogin.ts
@@ -3,23 +3,28 @@ import { useMutation } from "@tanstack/react-query";
 import Axios from "axios";
 import { toast } from "react-toastify";
 
-export interface RegisterPayload {
-  firstName: string;
-  lastName: string;
+export interface LoginPayload {
   username: string;
   password: string;
 }
 
-export const useRegister = () => {
-  // The useMutation hook takes 3 type parameters:
-  // 1. The response type (this route doesn't respond with any data on success, so it's void)
-  // 2. The error type
-  // 3. The payload type (if we are sending data)
-  return useMutation<void, ErrorResponse, RegisterPayload>(
-    ["/Users/register"],
+export interface LoginResponse {
+    id: number,
+    firstName: string,
+    lastName: string,
+    username: string,
+    token: string
+}
+
+export const useLogin = () => {
+
+    // TODO add auth context, logic here for setting token on successful login
+
+  return useMutation<LoginResponse, ErrorResponse, LoginPayload>(
+    ["/Users/login"],
     async (payload) => {
       const { data } = await Axios({
-        url: "/Users/register",
+        url: "/Users/login",
         method: "post",
         data: payload,
       });
@@ -27,7 +32,7 @@ export const useRegister = () => {
     },
     {
       onSuccess: (data) => {
-        toast.success("Registered successfully!");
+        toast.success(`Welcome, ${data.firstName}!`);
       },
       onError: ({ response}) => {
         toast.error(response?.data?.message ?? "Unknown Error.");
