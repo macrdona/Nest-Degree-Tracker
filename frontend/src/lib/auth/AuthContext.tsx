cf3da3375585a3fa9b2a48287333de5c8d4c5e17
Ricky LeDew <adotout1@gmail.com>
2023-04-03 10:23:47 -0400

    Edit jwt code

--- frontend/src/lib/auth/AuthContext.tsx
+++ frontend/src/lib/auth/AuthContext.tsx
@@ -1,69 +1,79 @@
-import { createContext, PropsWithChildren, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { User } from "../api/types";
-import jwt, { TokenExpiredError } from "jsonwebtoken"
+import { decodeToken, isExpired } from "react-jwt";
 import { toast } from "react-toastify";
 
 export interface AuthContextValue {
-    user?: User
-    token?: string
-    login: (token: string) => void
-    logout: () => void
-    loggedIn: boolean
+  user?: User;
+  token?: string;
+  login: (token: string) => void;
+  logout: () => void;
+  loggedIn: boolean;
 }
 
-const AuthContext = createContext<AuthContextValue>({login: () => {}, logout: () => {}, loggedIn: false});
+const AuthContext = createContext<AuthContextValue>({
+  login: () => {},
+  logout: () => {},
+  loggedIn: false,
+});
 
 export const useAuth = () => {
-    return useContext(AuthContext);
-}
+  return useContext(AuthContext);
+};
 
 export function AuthProvider(props: PropsWithChildren) {
-
   const [user, setUser] = useState<User>();
   const [token, setToken] = useState<string>();
   const [loggedIn, setLoggedIn] = useState<boolean>(false);
 
   const login = (token: string) => {
-    try {
-        const decoded = jwt.decode(token);
-
-        setUser(decoded as User);
-        setToken(token);
-        setLoggedIn(true);
-
-        toast.success("Logged in.");
-    } catch (err) {
-        toast.error("Invalid token, please log in again.");
-        logout();
+    const decoded: User | null = decodeToken(token) as User | null;
+    const expired = isExpired(token);
+    if (decoded && !expired) {
+      setUser(decoded);
+      setToken(token);
+      setLoggedIn(true);
+      console.log(decoded);
+      toast.success("Logged in.");
+    } else {
+      toast.error("Invalid token, please log in again.");
+      logout();
     }
-  }
+  };
 
   const logout = () => {
     localStorage.removeItem("token");
     setToken(undefined);
     setUser(undefined);
-    setLoggedIn(false);    
+    setLoggedIn(false);
+  };
 
-  }
-  
   useEffect(() => {
     if (!loggedIn) {
-        const storedToken = localStorage.getItem("token");
-        if (storedToken) {
-            login(storedToken)
-        }
+      const storedToken = localStorage.getItem("token");
+      if (storedToken) {
+        login(storedToken);
+      }
     }
-  }, [])
+  }, []);
 
   return (
-    <AuthContext.Provider value={{
+    <AuthContext.Provider
+      value={{
         loggedIn: loggedIn,
         logout: logout,
         token: token,
         login: login,
-        user: user
-    }}>
-        {props.children}
-    </AuthContext.Provider>);
+        user: user,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 }
-
