d83a8e185ee6bfc387422336f3fcd71342026827
kattypo <74074509+kattypo@users.noreply.github.com>
2023-04-14 00:46:50 -0400

    Step 3 of registration (WIP)

---
+++ frontend/src/components/RegisteringForms/OtherReqs.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './RegisterComplete.scss';
+import Steps3 from "../../assets/steps-image-3.png";
+
+function OtherReqs(){
+
+    return(
+        <div className="main container justify-content-center d-flex flex-column align-items-stretch">
+            <img src={Steps3}/>
+            //WIP
+        </div>
+        );
+}
+export default OtherReqs;
